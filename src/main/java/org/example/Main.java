06ccacd6b17c2506317825ea422495e9cc305d5a
rajashekharms369 <rajmkumar369@gmail.com>
2024-09-06 20:31:49 +0530

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Facade deisgn pattern
-
+        System.out.println("Hello world");
     }
     public int lengthOfLastWord(String s) {
         String[] strArray = s.split(" ");
