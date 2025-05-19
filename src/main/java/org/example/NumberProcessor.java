54ca9495e85af5db8c63a6e61b8571e6cf884ccd
rajashekharms369 <rajmkumar369@gmail.com>
2025-05-19 22:52:37 +0530

    reset

--- src/main/java/org/example/NumberProcessor.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public interface NumberProcessor {
-    void acceptVisitor(Visitor visitor);
-}
--- src/main/java/org/example/Visitor.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public interface Visitor {
-    // The method to visit the IntegerProcessor
-    void visitNumber(IntegerProcessor myInt);
-}
